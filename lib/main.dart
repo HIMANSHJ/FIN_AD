53f67a6ab93656d30858a1f316cb790e8d93ecd7
1himanshu <himanshu.joshi@mobcoder.com>
2020-04-10 00:08:28 +0530

    modified few files and db is working(app stable)

--- lib/main.dart
+++ lib/main.dart
@@ -24,173 +24,6 @@ class MyApp extends StatefulWidget {
   MainPage createState() => MainPage();
 }
 
-/*class MainPage extends State<MyApp> {
-  List<Todo> _todoList;
-
-  final FirebaseDatabase _database = FirebaseDatabase.instance;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-  final _textEditingController = TextEditingController();
-  StreamSubscription<Event> _onTodoAddedSubscription;
-  StreamSubscription<Event> _onTodoChangedSubscription;
-
-  Query _todoQuery;
-
-  //bool _isEmailVerified = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    //_checkEmailVerification();
-
-    _todoList = new List();
-    _todoQuery = _database
-        .reference()
-        .child("todo")
-        .orderByChild("userId")
-        .equalTo(widget.userId);
-    _onTodoAddedSubscription = _todoQuery.onChildAdded.listen(onEntryAdded);
-    _onTodoChangedSubscription =
-        _todoQuery.onChildChanged.listen(onEntryChanged);
-  }
-
-  @override
-  void dispose() {
-    _onTodoAddedSubscription.cancel();
-    _onTodoChangedSubscription.cancel();
-    super.dispose();
-  }
-
-  onEntryChanged(Event event) {
-    var oldEntry = _todoList.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-
-    setState(() {
-      _todoList[_todoList.indexOf(oldEntry)] =
-          Todo.fromSnapshot(event.snapshot);
-    });
-  }
-
-  onEntryAdded(Event event) {
-    setState(() {
-      _todoList.add(Todo.fromSnapshot(event.snapshot));
-    });
-  }
-
-  signOut() async {
-    try {
-      await widget.auth.signOut();
-      widget.logoutCallback();
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  addNewTodo(String todoItem) {
-    if (todoItem.length > 0) {
-      Todo todo = new Todo(todoItem.toString(), widget.userId, false);
-      _database.reference().child("todo").push().set(todo.toJson());
-    }
-  }
-
-  updateTodo(Todo todo) {
-    //Toggle completed
-    todo.completed = !todo.completed;
-    if (todo != null) {
-      _database.reference().child("todo").child(todo.key).set(todo.toJson());
-    }
-  }
-
-  deleteTodo(String todoId, int index) {
-    _database.reference().child("todo").child(todoId).remove().then((_) {
-      print("Delete $todoId successful");
-      setState(() {
-        _todoList.removeAt(index);
-      });
-    });
-  }
-  showAddTodoDialog(BuildContext context) async {
-    _textEditingController.clear();
-    await showDialog<String>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: new Row(
-              children: <Widget>[
-                new Expanded(
-                    child: new TextField(
-                  controller: _textEditingController,
-                  autofocus: true,
-                  decoration: new InputDecoration(
-                    labelText: 'Add new todo',
-                  ),
-                ))
-              ],
-            ),
-            actions: <Widget>[
-              new FlatButton(
-                  child: const Text('Cancel'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              new FlatButton(
-                  child: const Text('Save'),
-                  onPressed: () {
-                    addNewTodo(_textEditingController.text.toString());
-                    Navigator.pop(context);
-                  })
-            ],
-          );
-        });
-  }
-
-  Widget showTodoList() {
-    if (_todoList.length > 0) {
-      return ListView.builder(
-          shrinkWrap: true,
-          itemCount: _todoList.length,
-          itemBuilder: (BuildContext context, int index) {
-            String todoId = _todoList[index].key;
-            String subject = _todoList[index].subject;
-            bool completed = _todoList[index].completed;
-            String userId = _todoList[index].userId;
-            return Dismissible(
-              key: Key(todoId),
-              background: Container(color: Colors.red),
-              onDismissed: (direction) async {
-                deleteTodo(todoId, index);
-              },
-              child: ListTile(
-                title: Text(
-                  subject,
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                trailing: IconButton(
-                    icon: (completed)
-                        ? Icon(
-                            Icons.done_outline,
-                            color: Colors.green,
-                            size: 20.0,
-                          )
-                        : Icon(Icons.done, color: Colors.grey, size: 20.0),
-                    onPressed: () {
-                      updateTodo(_todoList[index]);
-                    }),
-              ),
-            );
-          });
-    } else {
-      return Center(
-          child: Text(
-        "Welcome. Your list is empty",
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30.0),
-      ));
-    }
-  }*/
-
 class MainPage extends State {
   @override
   Widget build(BuildContext context) {
@@ -261,7 +94,7 @@ class MainPage extends State {
                   RaisedButton(
                     textColor: Colors.white,
                     color: Colors.blue,
-                    child: Text('Sign Out'),
+                    child: Text('SIGN OUT'),
                     onPressed: () {
                       backToSignUpPage(context);
                     },
@@ -322,7 +155,19 @@ class SubPage extends StatelessWidget {
               ),
             ),
             ListTile(
-              title: Text('Policy Page'),
+              title: Text('NEW POLICY'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => ThirdPage()));
+              },
+            ),
+             ListTile(
+              title: Text('RENEW POLICY'),
               trailing: Icon(Icons.arrow_forward),
               onTap: () {
                 // Update the state of the app
@@ -334,7 +179,7 @@ class SubPage extends StatelessWidget {
               },
             ),
             ListTile(
-              title: Text('Agent Register'),
+              title: Text('AGENT REGISTRATION'),
               trailing: Icon(Icons.arrow_forward),
               onTap: () {
                 // Update the state of the app
@@ -373,7 +218,7 @@ class SubPage extends StatelessWidget {
             RaisedButton(
               textColor: Colors.white,
               color: Colors.blue,
-              child: Text('OLD POLICY'),
+              child: Text('RENEW POLICY'),
               onPressed: () {
                 navigateToThirdPage(context);
               },
@@ -406,15 +251,3 @@ class SubPage extends StatelessWidget {
     Navigator.pop(context);
   }
 }
-
-/*abstract class BaseAuth {
-
-  Future<void> signOut();
-  
-}
-
- class Auth implements BaseAuth {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-   Future<void> signOut() async {
-    return _firebaseAuth.signOut();
-  }}*/
